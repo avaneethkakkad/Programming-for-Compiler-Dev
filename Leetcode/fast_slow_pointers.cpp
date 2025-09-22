8ef484ed24d70e1596cea463e43839e4980b5439
DarkM@LLVM <avaneethkakkad@gmail.com>
2025-09-22 17:03:20 +0530

    Create fast_slow_pointers.cpp

---
+++ Leetcode/fast_slow_pointers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode{
+int val;
+ListNode* next;
+ListNode(int x):val(x),next(nullptr){}
+};
+
+ListNode* findMiddle(ListNode* head)
+{
+  ListNode* slow = head;
+  ListNode* fast = head;
+
+  while(fast!=nullptr && fast->next!=nullptr)
+{
+slow=slow->next;
+fast=fast->next->next;
+}
+return slow;
+}
