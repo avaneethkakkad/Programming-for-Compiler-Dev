51e3ebca53e3330cd3edf77837d6bd2c4ccdec2e
DarkM@LLVM <avaneethkakkad@gmail.com>
2025-10-05 11:04:30 +0530

    Update Dijkstra_algorithm.cpp

--- Datastructures/Graphs/Dijkstra_algorithm.cpp
+++ Datastructures/Graphs/Dijkstra_algorithm.cpp
@@ -1,4 +1,8 @@
 // User Function Template
+//Given an undirected, weighted graph with V vertices numbered from 0 to V-1 and E edges, represented by 2d array edges[][], where edges[i]=[u, v, w] represents the edge between the nodes u and v having w edge weight.
+//You have to find the shortest distance of all the vertices from the source vertex src, and return an array of integers where the ith element denotes the shortest distance between ith node and source vertex src.
+
+//Note: The Graph is connected and doesn't contain any negative weight edge.
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
